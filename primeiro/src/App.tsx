ef2af3ce55ab0736d2fb2291205103340aa4bd45
Ronier <ronier.cavalari@outlook.com>
2024-04-24 00:21:57 -0300

    Aula7 - useState
    Criando um input para capturar os value e exibir em div.

--- primeiro/src/App.tsx
+++ primeiro/src/App.tsx
@@ -1,16 +1,41 @@
-import { Header } from './components/header'
-import { Aluno } from './components/aluno'
-import { Footer } from './components/footer'
+import { useState } from 'react'
 
 export default function App(){
+
+  const [ input, setInput ] = useState(""); //a primeira variavel é o que vai no value do input
+  const [ idade, setIdade ] = useState<string | number>(""); // <string | number> faz aceitar qualquer valor se não seria somente string
+  const [ aluno, setAluno ] = useState("Sem nenhum nome"); 
+
+  function mostrarAluno(){
+    setAluno(input)
+    console.log(idade)
+  }
+
   return(
     <div>
-      <Header title='Alunos do React + TypeScript'/>
+      <h1>Conhecendo useState</h1>
 
-      <Aluno nome="Ronier" idade={23}/>
-      <Aluno nome="Maria" idade={12}/>
-      
-      <Footer/>
+      <input 
+        placeholder="Digite o nome"
+        value={input} //pega o useState
+        onChange={ (e) => setInput(e.target.value)} //sempre que digitado vai passar o valor no useState
+      />
+      <br />
+      <input 
+        placeholder="Digite a Idade"
+        value={idade} //pega o useState
+        onChange={ (e) => setIdade(e.target.value)} //sempre que digitado vai passar o valor no useState
+      />
+
+      <br />
+
+      <button onClick={mostrarAluno}>Mostrar Aluno</button>
+
+      <hr />
+
+      <div>
+        <h3>Bem Vindo: {aluno}</h3>
+      </div>
     </div>
   )
 }
--- primeiro/src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import './footer.css'
-
-export function Footer(){
-    return(
-        <footer>
-            <h4>Roni - Todos os Direitos Reservador</h4>
-        </footer>
-    )
-}
