c2ef88a748a150649affca98d8d1ddf7b7b0d6cf
Ronier <ronier.cavalari@outlook.com>
2024-04-24 00:41:54 -0300

    Aula8 - useState - contador
    Criando objeto com infoaluno.
    Criando contador.

--- primeiro/src/App.tsx
+++ primeiro/src/App.tsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
 
+interface InfoAlunoProps{
+  nome: string;
+  idade: string;
+}
+
 export default function App(){
 
   const [ input, setInput ] = useState(""); //a primeira variavel é o que vai no value do input
-  const [ idade, setIdade ] = useState<string | number>(""); // <string | number> faz aceitar qualquer valor se não seria somente string
-  const [ aluno, setAluno ] = useState("Sem nenhum nome"); 
+  const [ idade, setIdade ] = useState("");  
+  const [ contador, setContador ] = useState(0);
+
+  const [infoAluno, setInfoAluno] = useState<InfoAlunoProps>(); //Igual fiz na idade na aula anterior mas usando uma tipagem de interface
 
   function mostrarAluno(){
-    setAluno(input)
-    console.log(idade)
+    setInfoAluno({
+      nome: input,
+      idade: idade,
+    })
+  }
+
+  function adicionar(){
+    setContador(valorAtual => valorAtual + 1)
+  }  
+  function diminuir(){
+    if(contador === 0){
+      return
+    }
+    setContador(valorAtual => valorAtual - 1)
   }
 
   return(
@@ -20,22 +39,30 @@ export default function App(){
         value={input} //pega o useState
         onChange={ (e) => setInput(e.target.value)} //sempre que digitado vai passar o valor no useState
       />
-      <br />
+      <br /><br />
       <input 
         placeholder="Digite a Idade"
         value={idade} //pega o useState
         onChange={ (e) => setIdade(e.target.value)} //sempre que digitado vai passar o valor no useState
       />
 
-      <br />
+      <br /><br />
 
       <button onClick={mostrarAluno}>Mostrar Aluno</button>
 
       <hr />
 
       <div>
-        <h3>Bem Vindo: {aluno}</h3>
+        <h3>Bem Vindo: { infoAluno?.nome }</h3> 
+        <h4>Bem Vindo: { infoAluno?.idade }</h4>
+        {/* Acima ele coloca o ? para se tiver vazio não exibir nada */}
       </div>
+
+      <hr />
+      <br />
+
+      <h1>Contator com useState</h1>
+      <button onClick={adicionar}>+</button> {contador} <button onClick={diminuir}>-</button>
     </div>
   )
 }
