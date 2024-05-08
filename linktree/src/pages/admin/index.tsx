fdec3fa496929a96dc7a3f5a080ce7e2fbb2e101
Ronier <ronier.cavalari@outlook.com>
2024-05-07 23:57:09 -0300

    Aula49-Finalizando Formulario

--- linktree/src/pages/admin/index.tsx
+++ linktree/src/pages/admin/index.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { Header } from '../../components/header'
 import { Input } from '../../components/input'
 
+import { FiTrash } from 'react-icons/fi';
+
 export function Admin() {
   const [nameInput, setNameInput] = useState("");
   const [urlInput, setUrlInput] = useState("");
@@ -21,14 +23,7 @@ export function Admin() {
           onChange={(e) => setNameInput(e.target.value)}
         />
 
-        <label className='text-white font-medium mt-2 mb-2'>Nome do Link</label>
-        <Input
-          placeholder='Digite o nome do link...'
-          value={nameInput}
-          onChange={(e) => setNameInput(e.target.value)}
-        />
-
-        <label className='text-white font-medium mt-2 mb-2'>Nome do Link</label>
+        <label className='text-white font-medium mt-2 mb-2'>URL do Link</label>
         <Input
           type='url'
           placeholder='Digite a url...'
@@ -55,17 +50,41 @@ export function Admin() {
           </div>
         </section>
 
-        <div className='flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md'>
-          <label className='text-white font-medium mt-2 mb-3'>Veja como esta fincando</label>
-          <article 
-            className='w-11/12 max-w-lg flex flex-col items-center justify-between bg-slate-500 rounded px-1 py-3'
-            style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput}}
-          >
-            <p className="font-medium" style={{ color: textColorInput}} >Canal do Youtube</p>
-          </article>
-        </div>
+        {nameInput !== '' && (
+          <div className='flex items-center justify-center flex-col mb-7 p-1 border-gray-100/25 border rounded-md'>
+            <label className='text-white font-medium mt-2 mb-3'>Veja como esta fincando</label>
+            <article
+              className='w-11/12 max-w-lg flex flex-col items-center justify-between bg-slate-500 rounded px-1 py-3'
+              style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput }}
+            >
+              <p className="font-medium" style={{ color: textColorInput }} >{nameInput}</p>
+            </article>
+          </div>
+        )}
+
+        <button type='submit' className='mb-7 bg-blue-600 h-9 rounded-md font-medium gap-4 flex justify-center items-center'>
+          Cadastrar
+        </button>
 
       </form>
+
+      <h2 className='font-bold mb-4 text-2x1'>
+        Meus Links
+      </h2>
+
+      <article 
+        className="flex items-center justify-between w-11/12 max-w-xl rounded py-3 px-2 select-none"
+        style={{ backgroundColor: "#ccc", color: "#369" }}  
+      >
+        <p>Canal do Youtube</p>
+        <div>
+          <button
+            className="border border-dashed p-1 rounded bg-slate-700"
+            >
+            <FiTrash size={18} color='#ccc'/>
+          </button>
+        </div>
+      </article>
     </div>
   )
 }
