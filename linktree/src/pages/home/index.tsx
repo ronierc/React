da0448a80001cfe9eb2c9a0e1bc8a97fe53cc7f3
Ronier <ronier.cavalari@outlook.com>
2024-08-14 23:04:57 -0300

    removendo header da home

--- linktree/src/pages/home/index.tsx
+++ linktree/src/pages/home/index.tsx
@@ -72,7 +72,7 @@ export function Home() {
 
   return (
     <div className='flex flex-col w-full py-4 items-center justify-center'>
-        <Header/>
+      
         <h1 className='md:text-4xl text-3xl font-bold mt-20'>Sujeito Programador</h1>
         <span className='text-gray-50 mb-5 mt-3'>Veja meus links 👇</span>
 
